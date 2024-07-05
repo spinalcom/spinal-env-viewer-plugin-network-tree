c34ef0519b16fbc2692a284f017c156cc4f2dace
Laurent <l.ha-tam-dan@spinalcom.com>
2024-07-05 11:23:44 +0200

    fix select/isolate/fit button

--- src/buttons/network_buttons/seeSubItems.js
+++ src/buttons/network_buttons/seeSubItems.js
@@ -1,3 +1,27 @@
+/*
+ * Copyright 2024 SpinalCom - www.spinalcom.com
+ * 
+ * This file is part of SpinalCore.
+ * 
+ * Please read all of the following terms and conditions
+ * of the Software license Agreement ("Agreement")
+ * carefully.
+ * 
+ * This Agreement is a legally binding contract between
+ * the Licensee (as defined below) and SpinalCom that
+ * sets forth the terms and conditions that govern your
+ * use of the Program. By installing and/or using the
+ * Program, you agree to abide by all the terms and
+ * conditions stated or referenced herein.
+ * 
+ * If you do not agree to abide by these terms and
+ * conditions, do not demonstrate your acceptance and do
+ * not install or use the Program.
+ * You should have received a copy of the license along
+ * with this file. If not, see
+ * <http://resources.spinalcom.com/licenses.pdf>.
+ */
+
 import {
   SpinalContextApp,
   spinalContextMenuService,
@@ -57,22 +81,21 @@ class SeeSubItems extends SpinalContextApp {
 
     const bims = [node, ...children.map((el) => el.get())];
 
-    const ids = [];
+    const objs = [];
     const grouped = lodash.groupBy(bims, (item) => item.bimFileId);
     lodash.forEach(grouped, (value, key) => {
-      ids.push({
+      objs.push({
         bimFileId: key,
         dbIds: value.map((el) => el.dbid),
       });
     });
-
-    ids.forEach(({ bimFileId, dbIds }) => {
-      const model = spinal.BimObjectService.getModelByBimfile(bimFileId);
-      window.spinal.ForgeViewer.viewer.impl.visibilityManager.isolate(
-        dbIds,
-        model
-      );
+    const resItems = objs.map((el) => {
+      return {
+        model: spinal.BimObjectService.getModelByBimfile(el.bimFileId),
+        selection: el.dbIds,
+      };
     });
+    window.spinal.ForgeViewer.viewer.impl.visibilityManager.aggregateIsolate(resItems);
   }
 }
 
